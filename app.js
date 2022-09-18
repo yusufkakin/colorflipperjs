7c2acae944d94877afe592d16d1a51962030481d
Yusuf Akin <kutluhanakin@gmail.com>
2022-09-18 12:29:28 -0400

    finished

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const colorlist = ["blue", "red", "yellow", "green"]
+const colorlist = ["Blue", "Red", "Yellow", "Green"];
+
+let buttonChooser = document.querySelector(".clickbutton");
+let nameChooser = document.querySelector(".colorname");
+
+buttonChooser.addEventListener("click", function () {
+  let randomColor = colorRandomizer();
+
+  document.querySelector(".main").style.backgroundColor =
+    colorlist[randomColor];
+  nameChooser.textContent = colorlist[randomColor];
+});
+
+// get random number between 0-4
+let colorRandomizer = function () {
+  return Math.floor(Math.random() * colorlist.length);
+};
